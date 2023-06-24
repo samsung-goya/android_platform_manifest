1b7261c51f317ff8b0272b8ab607239b1558d7fd
Mehmet Fatih NASIR <134432548+mhmtnasir@users.noreply.github.com>
2023-06-24 18:24:38 +0300

    Update potf_goya.sh

--- potf_goya.sh
+++ potf_goya.sh
@@ -6,7 +6,7 @@
 # Patch 1: Fix prebuilts path
 rm -rf vendor/cm/get-prebuilts
 git clone --depth=1 https://github.com/samsung-goya/android_platform_manifest.git temp_manifest
-yes | cp -rf temp_manifest/potf/. .
+yes | cp -rf temp_manifest/potf_goya/. .
 rm -r temp_manifest
 source vendor/cm/get-prebuilts
 # ---
